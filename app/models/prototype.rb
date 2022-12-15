df35f182cbff53c5c373e22ad287ba687154967b
redmagic6s <ryodai.3168h@gmail.com>
2022-12-15 16:15:11 +0900

    投稿機能の実装

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,6 +1,6 @@
 class Prototype < ApplicationRecord
   belongs_to       :user
-  has_many         :comments, dependent: :destroy
+  # has_many         :comments, dependent: :destroy
   has_one_attached :image
 
   validates :title,      presence: true
@@ -8,3 +8,4 @@ class Prototype < ApplicationRecord
   validates :concept,    presence: true  
   validates :image,      presence: true
 end
+
